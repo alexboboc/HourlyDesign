380f917619c932807cc74fc34d0dac39fb36b1ce
Alex Boboc <me@alexboboc.com>
2019-02-11 01:15:37 +0100

    Added wait times for interaction

--- Dribbble.py
+++ Dribbble.py
@@ -15,12 +15,14 @@ class Dribbble(Provider):
     def get_popular(self):
         # Pick shot
         self.driver.get(self.url)
+        time.sleep(self.INTERACTION_SLEEP_TIME)
         elements = self.driver.find_elements_by_class_name("dribbble-over")
         shot = random.choice(elements)
         shot_url = shot.get_attribute("href")
  
         # Extract image url
         self.driver.get(shot_url)
+        time.sleep(self.INTERACTION_SLEEP_TIME)
         image = self.driver.find_element_by_css_selector(".main-shot picture source")
         image_url = image.get_attribute("srcset")
 
--- Provider.py
+++ Provider.py
@@ -2,6 +2,7 @@ class Provider:
 
     def __init__(self, url):
         self.url = url
+        self.INTERACTION_SLEEP_TIME = 3
 
     def get_pupular(self):
         print("[ERROR] Method get_popular not implemented in subclass.")
